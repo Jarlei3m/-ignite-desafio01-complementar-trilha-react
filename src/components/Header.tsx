90a114ecd8000ed2954da8c82b5d74997cbede23
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-03-19 18:22:13 -0300

    chore: Additional challenge 01 - completed

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+interface GenreResponseProps {
+  selectedGenre: {
+    title: string;
+  };
+}
+
+export function Header(props: GenreResponseProps) {
+  return (
+    <header>
+      <span className='category'>
+        Categoria:<span> {props.selectedGenre.title}</span>
+      </span>
+    </header>
+  );
+}
